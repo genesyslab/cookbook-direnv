81248f9b5c0987e40bd35fcf4c9a6c5ca608f636
Josh Mahowald <joshua.mahowald@gmail.com>
2016-08-16 02:25:32 +0000

    adding in name so it's compatible with berks

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer_email "hello@james.hu"
 license          "Apache 2.0"
 description      "Provides recipes for installing direnv."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
+name		 "direnv"
 
 recipe "direnv", "Installs direnv"
 
